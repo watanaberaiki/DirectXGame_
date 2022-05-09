12f6ad64ebbae140d10e8d10807c434881debdb7
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-05-09 10:43:50 +0900

    デバッグカメラの途中

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,98 +1,85 @@
 ﻿#include "GameScene.h"
-#include "Audio.h"
-#include "AxisIndicator.h"
-#include "DirectXCommon.h"
-#include "PrimitiveDrawer.h"
 #include "TextureManager.h"
-#include "WinApp.h"
+#include <cassert>
 
-// Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	WinApp* win = nullptr;
-	DirectXCommon* dxCommon = nullptr;
-	// 汎用機能
-	Input* input = nullptr;
-	Audio* audio = nullptr;
-	DebugText* debugText = nullptr;
-	AxisIndicator* axisIndicator = nullptr;
-	PrimitiveDrawer* primitiveDrawer = nullptr;
-	GameScene* gameScene = nullptr;
+GameScene::GameScene() {}
 
-	// ゲームウィンドウの作成
-	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+GameScene::~GameScene() { 
+	delete model_; 
+	delete debugCamera_;
+}
 
-	// DirectX初期化処理
-	dxCommon = DirectXCommon::GetInstance();
-	dxCommon->Initialize(win);
+void GameScene::Initialize() {
 
-#pragma region 汎用機能初期化
-	// 入力の初期化
-	input = Input::GetInstance();
-	input->Initialize();
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	
+	//3Dモデルの生成
+	model_ = Model::Create();
 
-	// オーディオの初期化
-	audio = Audio::GetInstance();
-	audio->Initialize();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
 
-	// テクスチャマネージャの初期化
-	TextureManager::GetInstance()->Initialize(dxCommon->GetDevice());
-	TextureManager::Load("white1x1.png");
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280,720);
+}
 
-	// スプライト静的初期化
-	Sprite::StaticInitialize(dxCommon->GetDevice(), WinApp::kWindowWidth, WinApp::kWindowHeight);
+void GameScene::Update() {
+	//
+}
 
-	// デバッグテキスト初期化
-	debugText = DebugText::GetInstance();
-	debugText->Initialize();
+void GameScene::Draw() {
 
-	// 3Dモデル静的初期化
-	Model::StaticInitialize();
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
-	// 軸方向表示初期化
-	axisIndicator = AxisIndicator::GetInstance();
-	axisIndicator->Initialize();
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
 
-	primitiveDrawer = PrimitiveDrawer::GetInstance();
-	primitiveDrawer->Initialize();
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
 #pragma endregion
 
-	// ゲームシーンの初期化
-	gameScene = new GameScene();
-	gameScene->Initialize();
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
 
-	// メインループ
-	while (true) {
-		// メッセージ処理
-		if (win->ProcessMessage()) {
-			break;
-		}
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	//3Dモデル描画
+	model_->Draw(worldTransform_,viewProjection_,textureHandle_);
 
-		// 入力関連の毎フレーム処理
-		input->Update();
-		// ゲームシーンの毎フレーム処理
-		gameScene->Update();
-		// 軸表示の更新
-		axisIndicator->Update();
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
 
-		// 描画開始
-		dxCommon->PreDraw();
-		// ゲームシーンの描画
-		gameScene->Draw();
-		// 軸表示の描画
-		axisIndicator->Draw();
-		// プリミティブ描画のリセット
-		primitiveDrawer->Reset();
-		// 描画終了
-		dxCommon->PostDraw();
-	}
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
 
-	// 各種解放
-	SafeDelete(gameScene);
-	audio->Finalize();
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
 
-	// ゲームウィンドウの破棄
-	win->TerminateGameWindow();
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
 
-	return 0;
+#pragma endregion
 }
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -47,60 +47,20 @@ class GameScene {
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	uint32_t textureHandle_ = 0;
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
 
 	//デバッグカメラ
 	DebugCamera* debugCamera_ = nullptr;
 
-	//立方体
-	Vector3 vec3;
-
-	Vector3 objectVertex[8];
-
-	Vector3 moveObjectVertex[8];
-
-	Vector3 scaleObjectVertex[8];
-
-	Vector3 rotateObjectVertex[8];
-
-	int edgeList[12][2] = {
-	  {0, 1},
-      {1, 2},
-      {2, 3},
-      {3, 0},
-      {0, 4},
-      {1, 5},
-	  {2, 6},
-      {3, 7},
-      {4, 5},
-      {5, 6},
-      {6, 7},
-      {7, 4}
-    };
-
-	//平行移動宣言
-	float move[4][4] = {
-	  {1.0f, 0.0f, 0.0f, 20.0f},
-	  {0.0f, 1.0f, 0.0f, 20.0f},
-	  {0.0f, 0.0f, 1.0f, 20.0f},
-	  {0.0f, 0.0f, 0.0f, 1.0f }
-    };
-
-	//拡大縮小宣言
-	float scale[4][4] = {
-	  {2.0f, 0.0f, 0.0f, 0.0f},
-	  {0.0f, 2.0f, 0.0f, 0.0f},
-	  {0.0f, 0.0f, 2.0f, 0.0f},
-	  {0.0f, 0.0f, 0.0f, 1.0f}
-    };
-
-	//回転宣言
-	double rotate[4][4] = {
-	  {cos(3.14 / 4), -sin(3.14 / 4), 0.0f, 0.0f},
-	  {sin(3.14 / 4), cos(3.14 / 4),  0.0f, 0.0f},
-	  {0.0f,          0.0f,           1.0f, 0.0f},
-	  {0.0f,          0.0f,           0.0f, 1.0f}
-    };
-
+	
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
